28560fdf63c525e05ed02946d80ffa0f86770cf7
Ann1ks <40628788+Ann1ks@users.noreply.github.com>
2020-06-04 20:11:33 +0300

    Add files via upload

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,24 +1,65 @@
 #include <iostream>
-#include "chset.h"
+#include <math.h>
+#include <vector>
 
-int main()
-{
-	chset set1;
-	chset set2;
-	chset set3;
-	set1.input(3);
-	set2.input(3);
-	set1.print();
-	set2.print();
-	set1 = set1 - 'A';
-	set2 = set2 - 'A';
-	set3 = set1 * set2;
-	set1.print();
-	set2.print();
-	set3.print();
-	if (set1 < set2)
-		std::cout << "Set1 < set2" << std::endl;
-	else
-		std::cout << "Set1 >= set2" << std::endl;
+int main() {
+	setlocale(0, "RUS");
+	std::vector<double> values;
+	std::vector<double> results;
+	double summ = 0;
+	int countNeg = 0;
+	rand();
+	std::vector<double>::iterator iter, firstNeg, secondNeg;
+	for (int i = 0; i < 10; i++)
+		if (i % 5 == 0)
+			values.push_back(0);
+		else
+			values.push_back(-10 + rand() % 20);
+	for (double element : values) // Добавляем в список отрицательные элементы
+		if (element < 0)
+			results.push_back(element);
+	for (double element : values) // Добавляем в список нулевые элементы
+		if (element == 0)
+			results.push_back(element);
+	for (double element : values) // Добавляем в список положительные элементы
+		if (element > 0)
+			results.push_back(element);
+
+	std::cout << "Исходная последовательность:" << std::endl;
+	for (int i = 0; i < values.size(); i++)
+		std::cout << values[i] << " | ";
+	std::cout << std::endl;
+
+	std::cout << "Задание #1:" << std::endl;
+	for (int i = 0; i < results.size(); i++)
+		std::cout << results[i] << " | ";
+
+	firstNeg = values.begin();
+	secondNeg = values.begin();
+	for (iter = values.begin(); iter != values.end(); iter++) { // Ищем первое и второе отрицательное значение
+		if (*iter < 0) {
+			switch (countNeg)
+			{
+			case 0:
+				firstNeg = iter;
+				countNeg++;
+				break;
+			case 1:
+				secondNeg = iter;
+				countNeg++;
+				break;
+			default:
+				break;
+			}
+			if (countNeg >= 2)
+				break;
+		}
+	}
+	std::cout << "\nПервое отр.: " << *firstNeg << "; Второе отр.: " << *secondNeg << std::endl;
+	for (iter = ++firstNeg; iter < secondNeg; iter++) {
+		summ += *iter;
+	}
+
+	std::cout << "\nЗадание #2(Используется исходная последовательность). Сумма: " << summ << std::endl;
 	return 0;
 }
